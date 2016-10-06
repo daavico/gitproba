0800e24205d1c243a1bde28dee2af12d0d41981b
alumne1daw <deividvicus96@gmail.com>
2016-10-06 16:16:25 +0200

    Versio1

---
+++ Girar num/src/girar.java
@@ -0,0 +1,29 @@
+								
+public class girar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String numero ="234";
+		String numeronou="";
+		
+		int numeroo= Integer.parseInt(numero);
+		
+		int contador=numero.length();
+		
+		System.out.println("Pepinissimo");
+		
+		
+		for(int i=0;i<numero.length();i++){
+			
+			numeronou=numero.substring(1) + numero.charAt(0);
+		
+		}
+		System.out.println(numeronou);
+		System.out.println("El maxim de combinacions es de:" +contador);
+			
+	
+	}
+}
+
